4322a87ad0bff12bad3ae289c02590e9047dcb70
Petr S <petr.strasik@gmail.com>
2022-10-09 22:52:19 +0200

    úprava češtiny v zadání v main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-"""zadani domaciho ukolu do pristi soboty tj. 15.09.2022: napiste funkci validujici rodne cislo, funkce bere jako
-parameter string s rodnym cislem, a vrati True nebo False v zavislosti na vysledku validace. K funkci pridejte
-positivni a negativni Pytest test case. Zkuste použit programování s cyklem. """
+""" zadání domácího úkolu do příští soboty tj. 15.09.2022:
+    napište funkci validující rodné číslo, funkce bere jako parameter string s rodným číslem a vrátí True nebo False
+    v závislosti na výsledku validace. K funkci přidejte pozitivní a negativní Pytest test case.
+    Zkuste použit programování s cyklem. """
