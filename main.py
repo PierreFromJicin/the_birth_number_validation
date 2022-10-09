d0ac466ac2171e8c30d3b6aadac9a5315c906b03
Petr S <petr.strasik@gmail.com>
2022-10-09 22:07:53 +0200

    úprava .gitignore a zadání úlohy v main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-'''
-zadani domaciho ukolu do pristi soboty tj. 15.09.2022:
-napiste funkci validujici rodne cislo, funkce bere jako parameter string s rodnym cislem, a vrati True nebo False v zavislosti na vysledku validace.
-K funkci pridejte positivni a negativni Pytest test case.
-Zkuste použit programování s cyklem.
-'''
+"""zadani domaciho ukolu do pristi soboty tj. 15.09.2022: napiste funkci validujici rodne cislo, funkce bere jako
+parameter string s rodnym cislem, a vrati True nebo False v zavislosti na vysledku validace. K funkci pridejte
+positivni a negativni Pytest test case. Zkuste použit programování s cyklem. """
