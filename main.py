ceb1097a4efe81c068019f432ef5c4b0abfd7105
Petr S <Petr.Strasik@gmail.com>
2022-10-10 01:32:37 +0200

    napsána f-ce slash_find v modulu string_parsing.py a rozhodování na její výstup v main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import string_parsing
 cl_in: str = input('enter the birth number (you can use slash separator or not) --> ')
 
 slash_index = string_parsing.slash_find(cl_in)
+
+if slash_index == -1:
+    print('has been entered birth number without slash')
+else:
+    print(f"Slash is on index:  {slash_index} in the string.")
--- string_parsing.py
+++ string_parsing.py
@@ -1,2 +1,13 @@
 def slash_find(str_in=""):
-    pass
+    # str_in_len = len(str_in)
+    print(len(str_in))
+    li_str_in = list(str_in)
+    print(li_str_in)
+
+    for _i in range(len(str_in)):
+        print(_i)
+        if li_str_in[_i] != chr(0x2f):
+            continue
+        else:
+            return _i
+    return -1
