ab04bf5463bc237ab0616154b8b7edd06df112ea
Petr S <61059011+PierreFromJicin@users.noreply.github.com>
2022-10-13 00:20:46 +0200

    Update main.py
    převod list stringu na list číslic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import string_parsing
+import string_parsing,*
 
 cl_in: str = input('enter the birth number (you can use slash separator or not) --> ')
 
@@ -8,3 +8,20 @@ if slash_index == -1:
     print('has been entered birth number without slash')
 else:
     print(f"Slash is on index:  {slash_index} in the string.")
+
+
+# převod list stringu na list číslic
+li_num = []
+def list_convertor(li_str_in):
+    for _i in range(len(li_str_in)):
+        if li_str_in[_i] isdigit:
+            li_num += int(li_str_in[_i])
+            continue
+        else:
+            return _i
+            break
+    return li_num
+
+
+li_num = list_convertor(li_str_in)
+print(f"list čísel: {li_num})
