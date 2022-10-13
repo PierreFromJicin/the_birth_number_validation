4eb011ba471de0a1ca52934d2be321678a468c6c
Petr S <61059011+PierreFromJicin@users.noreply.github.com>
2022-10-13 18:14:15 +0200

    update number_parsing.py

--- number_parsing.py
+++ number_parsing.py
@@ -18,21 +18,21 @@ def list_convertor(*tu_str):
 def num_validator(num_list):
 	i = num_list
 	if i[2] != 0:
-		if not((i[2] = 1) or (i[2] = 6)):
-			if i[2] = 5:
+		if not((i[2] == 1) or (i[2] == 6)):
+			if i[2] == 5:
 				if i[4] != 1:
 					if i[4] != 3:
-						if i[4] = 2:
+						if i[4] == 2:
 							if i[5] != 9:
-								if len(i)=10:
+								if len(i)==10:
 									for _i in range(0, 9):
 										# n *= 10
 										n = n*10+i[_i]
-									if n // 11 = 0:
+									if n // 11 == 0:
 										return True
 										else:
 											return False
-								elif len(i)=9:
+								elif len(i)==9:
 									for _i in range(6, 8):
 										# n *= 10
 										ns = ns*10+i[_i]
