628545b75f521648ed54ccdb620db62cde139079
Petr S <Petr.Strasik@gmail.com>
2022-10-14 00:27:21 +0200

    Update number_parsing.py
    num_validator

--- number_parsing.py
+++ number_parsing.py
@@ -13,85 +13,56 @@ def list_convertor(*tu_str):
     return li_num
 
 
-def inner_z(inli):
-	i = inli
-	if len(i)==10:				# length is 10
-		for _i in range(0, 9):
-			# n *= 10
-			n = n*10+i[_i]
-		if n // 11 == 0:		# 
-			return True
-		else:
-			return False
-	elif len(i)==9:				# length is 9
-		for _i in range(6, 8):
-			# n *= 10
-			ns = ns*10+i[_i]
-		if ns == 0:
-			return True
-		else:
-			return False
-	else:
-		return False
+def end_f(param):
+    i = param
+    n = 0
+    if len(i) == 10:  # length is 10
+        for _i in range(0, 9):
+            # n *= 10
+            n = n * 10 + i[_i]
+        if n // 11 == 0:  # modulo 11 must be 0
+            return True
+        else:
+            return False
+    elif len(i) == 9:  # length is 9
+        for _i in range(6, 8):
+            # n *= 10
+            n = n * 10 + i[_i]
+        if n == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
 
-
-# TODO samotný algoritmus analýzy (ify) a výstup True/False (this file)
 
 def num_validator(num_list):
-	i = num_list
-	if i[2] != 0:
-		if not((i[2] == 1) or (i[2] == 6)):
-			if i[2] == 5:
-				if i[4] != 1:
-					if i[4] != 3:
-						if i[4] == 2:
-							if i[5] != 9:
-								if len(i)==10:
-									for _i in range(0, 9):
-										# n *= 10
-										n = n*10+i[_i]
-									if n // 11 == 0:
-										return True
-										else:
-											return False
-								elif len(i)==9:
-									for _i in range(6, 8):
-										# n *= 10
-										ns = ns*10+i[_i]
-										if ns == 0:
-											return True
-										else:
-											return False
-									else:
-										return False
-								else:
-									for _i in range(0, 1):
-										# n *= 10
-										n = n*10+i[_i]
-									if n // 4 == 0:
-										# TODO opakování kódu (poslední fáze)
-										'''
-										if len(i)==10:
-									for _i in range(0, 9):
-										# n *= 10
-										n = n*10+i[_i]
-									if n // 11 == 0:
-										return True
-										else:
-											return False
-								elif len(i)==9:
-									for _i in range(6, 8):
-										# n *= 10
-										ns = ns*10+i[_i]
-										if ns == 0:
-											return True
-										else:
-											return False
-									else:
-										return False
-								else:
-										'''
-									return False
-							else:
-								return False
-    return False
+    i = num_list
+    n = 0
+    if i[2] != 0:
+        if not ((i[2] == 1) or (i[2] == 6)):
+            if i[2] == 5:
+                if i[4] != 1:
+                    if i[4] != 3:
+                        if i[4] == 2:
+                            if i[5] == 9:
+                                for _i in range(0, 1):
+                                    n = n * 10 + i[_i]
+                                if n // 4 == 0:
+                                    return end_f(i)
+                                else:
+                                    return False
+                            else:
+                                return False
+                        else:
+                            return False
+                    else:
+                        return False
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
