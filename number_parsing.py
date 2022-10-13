8a0c663bcde2ad2084fbb5cc07c64412c920d9b9
Petr S <61059011+PierreFromJicin@users.noreply.github.com>
2022-10-13 17:55:19 +0200

    update number_parsing.py

--- number_parsing.py
+++ number_parsing.py
@@ -25,13 +25,21 @@ def num_validator(num_list):
 						if i[4] = 2:
 							if i[5] != 9:
 								if len(i)=10:
-									for _i in range(0, 910):
+									for _i in range(0, 9):
 										# n *= 10
 										n = n*10+i[_i]
 									if n // 11 = 0:
 										return True
-									
-									else:
-										False
-									
+										else:
+											return False
+								elif len(i)=9:
+									for _i in range(6, 8):
+										# n *= 10
+										ns = ns*10+i[_i]
+										if ns == 0:
+											return True
+										else:
+											False
+										
+								
     return False
