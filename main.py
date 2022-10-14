829fdf5bbd42fc706c276cfe2bc7b5f5b1433e62
Petr S <Petr.Strasik@gmail.com>
2022-10-14 02:35:50 +0200

    final version 1.0.1 - updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from number_parsing import *
 from string_parsing import *
 
-cl_in: str = input('enter the birth number (you can use slash separator or not) --> ')
+cl_in: str = input('enter the birth number (you can use the slash separator if you want) --> ')
 
 slash_index = slash_find(cl_in)
 
@@ -13,19 +13,17 @@ else:
 '''
 
 tu_str_in = slash_find(cl_in)
-# print(f"tu_str_in[1]: {tu_str_in[1]}")
 birth_num = list_convertor(tu_str_in[1])
-# print(f"list čísel: {birth_num}")
 
-# TODO write num_validator in number_parsing module, according flowchart
+# NOTE num_validator in number_parsing module has been written according flowchart included in git repo.
 
 if birth_num:
     if len(birth_num) == 10 or len(birth_num) == 9:
         if num_validator(birth_num):
-            print(f"The birth number is valid.")
+            print(f"The birth number is VALID.")
         else:
-            print("The birth number is not valid.")
+            print("The birth number is NOT VALID.")
     else:
-        print(f"Wrong length of the entered birth hnumber: {len(birth_num)}")
+        print(f"Wrong length of the entered birth number: {len(birth_num)}")
 else:
-    print(f"Wrong format of the entered birth hnumber: {birth_num}")
+    print(f"Wrong format of the entered birth number: {birth_num}")
